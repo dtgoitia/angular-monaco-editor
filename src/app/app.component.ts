bf74b620768a1c19fd77b4df98df81b3dcd138a4
David Torralba Goitia <david.torralba.goitia@gmail.com>
2018-04-20 08:13:16 +0100

    Schema implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,27 @@
 import { Component } from '@angular/core';
+import { NgxEditorModel  } from 'ngx-monaco-editor';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'Monaco Editor';
   editorOptions = {
-    theme: 'vs-dark',
-    language: 'json'
+    // theme: 'vs-dark',
+    theme: 'vs-light',
+    language: 'markdown'
   };
-  code = 'function x() {\n\tconsole.log("Hello world!");\n}';
+  editorContent = `{
+  "p1": 123,
+  "p2": true,
+  "p3": "blah"
+}`;
+  editorModel: NgxEditorModel = {
+    value: this.editorContent,
+    language: 'json',
+    uri: 'foo.json'
+  };
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,70 @@ import { FormsModule } from '@angular/forms';
 
 /* components */
 import { AppComponent } from './app.component';
-import { MonacoEditorModule } from 'ngx-monaco-editor';
+import { MonacoEditorModule, NgxMonacoEditorConfig  } from 'ngx-monaco-editor';
 
+
+const monacoConfig: NgxMonacoEditorConfig = {
+  onMonacoLoad: () => {
+    const id = 'foo.json';
+    monaco.languages.json.jsonDefaults.setDiagnosticsOptions({
+      validate: true,
+      allowComments: false,
+      schemas: [{
+        uri: 'http://www.mocky.io/v2/5ad98cf42f00004800cfddb9',
+        fileMatch: [id],
+        schema: {
+          'type': 'object',
+          'properties': {
+            'p1': {
+              'type': 'number'
+            },
+            'p2': {
+              'type': 'boolean'
+            },
+            'p3': {
+              'type': 'string'
+            },
+            'nodes': {
+              'type': 'array',
+              'minItems': 1,
+              'items': {
+                'anyOf': [
+                  {
+                    '$ref': '#/definitions/nodes/blah'
+                  }
+                ]
+              }
+            }
+          },
+          'additionalProperties': false,
+          'required': [
+            'p1',
+            'p2',
+            'p3',
+            'nodes'
+          ],
+          'definitions': {
+            'nodes': {
+              'blah': {
+                'type': 'object',
+                'properties': {
+                  'thing': {
+                    'type': 'string'
+                  },
+                  'value': {
+                    'type': 'number'
+                  }
+                },
+                'additionalProperties': false
+              }
+            }
+          }
+        }
+      }]
+    });
+  }
+};
 
 @NgModule({
   declarations: [
@@ -14,7 +76,7 @@ import { MonacoEditorModule } from 'ngx-monaco-editor';
   imports: [
     BrowserModule,
     FormsModule,
-    MonacoEditorModule.forRoot()
+    MonacoEditorModule.forRoot(monacoConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
